4b3e0f88717b51fa12f1bb6fca8a00c8efab7f1b
Kirito <iiskondra11@yandex.ru>
2025-10-08 11:58:55 +0300

    Работающий калькулятор без обработки ошибок (принимает только правильные арифм выражения)

--- src/parser.py
+++ src/parser.py
@@ -6,10 +6,14 @@ def parse_expression(tokens: list) -> float:
     now_token_index = 0
     for index in range(len(tokens)):
         token = tokens[index]
-        if all(x in string.digits for x in token):
+        try:
             tokens[index] = float(token)
+        except ValueError:
+            continue
     print(tokens)
-    result = add_function(tokens, now_token_index)
+    result, now_token_index = add_function(tokens, now_token_index)
+    if now_token_index != len(tokens):
+        raise ValueError("ERROR")
     return result
 '''
     if tokens.count("(") != tokens.count(")"):
@@ -35,85 +39,86 @@ def parse_expression(tokens: list) -> float:
     return add_function(tokens, now_token)
 '''
 # Функция
-def add_function(tokens: list, now_token_index: int) -> float:
-    left_token = mul_function(tokens, now_token_index)
-    while now_token_index < len(tokens):
-        if tokens[now_token_index] in OPERATORS_ADD:
-            operation = tokens[now_token_index]
-            now_token_index += 1
-            right_token = mul_function(tokens, now_token_index)
-            left_token = add_operation(left_token, operation, right_token)
-    return left_token
+def add_function(tokens: list, now_token_index: int) -> tuple[float, int]:
+    left_token, now_token_index = mul_function(tokens, now_token_index)
+    while now_token_index < len(tokens) and tokens[now_token_index] in OPERATORS_ADD:
+        operation = tokens[now_token_index]
+        now_token_index += 1
+        right_token, now_token_index = mul_function(tokens, now_token_index)
+        left_token = add_operation(left_token, operation, right_token)
+    return left_token, now_token_index
 
-def mul_function(tokens: list, now_token_index: int) -> float:
-    left_token = power_function(tokens, now_token_index)
-    while now_token_index < len(tokens):
-        if tokens[now_token_index] in OPERATORS_MUL:
-            operator = tokens[now_token_index]
-            now_token_index += 1
-            right_token = power_function(tokens, now_token_index)
-            if operator in '/%//' and right_token == '0.0':
-                raise ZeroDivisionError("ДЕЛИТЬ НА НОЛЬ НЕЛЬЗЯ, БИТЮКОВ НАКАЖЕТ!!!")
-            if operator in '//%':
-                if ((str(right_token)[-2] + str(right_token)[-1]) in '.0') and (str(right_token)[-2] + str(right_token)[-1]) in '.0':
-                    left_token = mul_operation_integer(left_token, operator, right_token)
-                else:
-                    raise ZeroDivisionError("НЕЛЬЗЯЯЯЯЯ")
-            else:
+def mul_function(tokens: list, now_token_index: int) -> tuple[float, int]:
+    left_token, now_token_index = power_function(tokens, now_token_index)
+    while now_token_index < len(tokens) and tokens[now_token_index] in OPERATORS_MUL:
+        operator = tokens[now_token_index]
+        now_token_index += 1
+        right_token, now_token_index = power_function(tokens, now_token_index)
+        if operator == '*':
+            left_token = mul_operation_float(left_token, operator, right_token)
+        if operator == '/':
+            if right_token != 0.0:
                 left_token = mul_operation_float(left_token, operator, right_token)
+            else:
+                raise ZeroDivisionError("ДЕЛЕНИЕ НА НОЛЬ")
+        if operator == '//' or operator == '%':
+            if not right_token.is_integer() or not left_token.is_integer():
+                raise TypeError("ТОЛЬКО ДЛЯ ЦЕЛЫХ")
+            left_token =float(mul_operation_integer(int(left_token), operator, int(right_token)))
 
-    return left_token
+    return left_token, now_token_index
 
 
-def power_function(tokens: list, now_token_index: int) -> float:
-    left_token = unary_function(tokens, now_token_index)
-    while now_token_index < len(tokens):
+def power_function(tokens: list, now_token_index: int) -> tuple[float, int]:
+    left_token, now_token_index = unary_function(tokens, now_token_index)
+    if  now_token_index < len(tokens): # Ставим if, потому что вызываем потом саму функцию еще раз
         if tokens[now_token_index] in OPERATORS_POW:
             now_token_index += 1
-            right_token = power_function(tokens, now_token_index)
+            right_token, now_token_index = power_function(tokens, now_token_index)
             left_token = left_token ** right_token
-    return left_token
+    return left_token, now_token_index
 
 
-def unary_function(tokens: list, now_token_index: int) -> float:
+def unary_function(tokens: list, now_token_index: int) -> tuple[float,int]:
     if tokens[now_token_index] in UNARY_OPERATORS:
         unary = tokens[now_token_index]
         now_token_index += 1
-        digit = primary_function(tokens, now_token_index)
+        digit, now_token_index = primary_function(tokens, now_token_index)
         if unary == '+':
-            return digit
+            return digit, now_token_index
         elif unary == '-':
-            return -digit
+            return -digit, now_token_index
 
     return primary_function(tokens, now_token_index)
 
 
-def primary_function(tokens: list, now_token_index: int) -> float:
+def primary_function(tokens: list, now_token_index: int) -> tuple[float, int]:
     now_token = tokens[now_token_index]
     if now_token == '(':
         now_token_index += 1
-        result = add_function(tokens, now_token_index)
+        result, now_token_index = add_function(tokens, now_token_index)
         if (len(tokens) <= now_token_index) or tokens[now_token_index]  != ')':
             raise ValueError("СКОБКИ НЕ ЗАКРЫТЫ")
         now_token_index += 1
-        return result
+        return result, now_token_index
     else:
-        now_token_index += 1
         try:
-            return float(tokens[now_token_index])
+            result = float(tokens[now_token_index])
         except ValueError:
-            return 0.0
+            raise ValueError("ERROR")
+
+    return result, now_token_index+1
 
 
 def mul_operation_float(left_token: float, operator, right_token: float) -> float:
-    if operator in '*': return left_token * right_token
+    if operator == '*': return left_token * right_token
     else: return left_token / right_token
 
-def mul_operation_integer(left_token: float, operator, right_token: float) -> float:
-    if operator in '//': return left_token // right_token
+def mul_operation_integer(left_token: int, operator, right_token: int) -> int:
+    if operator == '//': return left_token // right_token
     else: return left_token % right_token
 
 def add_operation(left_token, operation, right_token) -> float:
-    if operation in '+': return left_token + right_token
+    if operation == '+': return left_token + right_token
     else: return left_token - right_token
 
