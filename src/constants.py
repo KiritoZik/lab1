96f85de476681888de8072497a224eecbce66199
Kirito <iiskondra11@yandex.ru>
2025-10-08 18:07:11 +0300

    Добавил обработку подряд идущих операторов. Изменил константы-словари с операторами на обычные списки без реализации операций.

--- src/constants.py
+++ src/constants.py
@@ -1,20 +1,12 @@
-import operator
+OPERATORS_ADD = ["+", "-"]
 
-OPERATORS_ADD = {
-    "+": operator.add,
-    "-": operator.sub
-}
+OPERATORS_MUL=["*", "/", "//", "%"]
 
-OPERATORS_MUL={
-    "*": operator.mul,
-    "/": operator.truediv,
-    "//": operator.floordiv,
-    "%": operator.mod
-}
+OPERATORS_POW = ["**"]
 
-OPERATORS_POW = {
-    "**": operator.pow,
-}
+UNARY_OPERATORS = ["+", "-"]
 
-UNARY_OPERATORS = {"+", "-"}
-
+ERROR_COMBINATION = ['%+', '%-', '%/', '%*', '%//', '%**', '+%', '+-', '+/',
+                     '+*', '+//', '+**','-%', '-+', '-/', '-*', '-//', '-**', '/%', '/+', '/-', '/*', '///',
+                     '/**', '*%', '*+', '*-', '*/', '*//', '***', '//%', '//+', '//-', '///', '//*', '//**',
+                     '**%', '**+', '**-', '**/', '***', '**//', '++', '--', '////', '%%', '****']
--- src/parser.py
+++ src/parser.py
@@ -1,8 +1,11 @@
-from src.constants import OPERATORS_ADD, OPERATORS_MUL, OPERATORS_POW, UNARY_OPERATORS
-import string
-# Основная функция для вычисления значения выражения.
-def parse_expression(tokens: list) -> float:
+from src.constants import OPERATORS_ADD, OPERATORS_MUL, OPERATORS_POW, UNARY_OPERATORS, ERROR_COMBINATION
 
+
+'''Основная функция для вычисления значения выражения, \
+с которого начинается рекурсивный спуск'''
+def parse_expression(tokens: list) -> float:
+    if any(op in ''.join(tokens) for op in ERROR_COMBINATION):
+        raise SyntaxError("ERROR")
     now_token_index = 0
     for index in range(len(tokens)):
         token = tokens[index]
@@ -10,34 +13,12 @@ def parse_expression(tokens: list) -> float:
             tokens[index] = float(token)
         except ValueError:
             continue
-    print(tokens)
     result, now_token_index = add_function(tokens, now_token_index)
     if now_token_index != len(tokens):
         raise ValueError("ERROR")
     return result
-'''
-    if tokens.count("(") != tokens.count(")"):
-        return "Ошибка"
 
-    copy_tokens = ''.join(tokens[::])
-    brackets= [] # список, состоящий из кортежей - индексов открывающих скобок и соответствующих им закрывающих скобок
-    while copy_tokens.find('(') != -1:
-        brackets.append((copy_tokens.find('('), copy_tokens.rfind(')')))
-        # Удаляем записанные скобки посредством замены на символ пробела, чтобы индексация не нарушилась
-        copy_tokens = copy_tokens[:copy_tokens.find('(')] + ' ' + copy_tokens[copy_tokens.find('(')+1:copy_tokens.rfind(')')] + ' ' + copy_tokens[copy_tokens.rfind(')')+1:]
-    brackets = brackets[::-1] # Переворачиваем, чтобы слева направо стояли скобки от внутренних к внешним
 
-    tmp_list = [] # Создаем пустой массив, где буду храниться кортежи - (арифметическое значение выражения в скобках, \
-    # индекс открывающей скобки, индекс закрывающей скобки
-    for poz in brackets:
-        now_token = 0
-        tmp = add_function(tokens[poz[0]+1:poz[1]], now_token)
-        tmp_list.append((tmp, poz[0], poz[1]))
-    for res, poz1, poz2 in tmp_list:
-        tokens = tokens[:poz1] + res + tokens[poz2+1:]
-    now_token = 0
-    return add_function(tokens, now_token)
-'''
 # Функция
 def add_function(tokens: list, now_token_index: int) -> tuple[float, int]:
     left_token, now_token_index = mul_function(tokens, now_token_index)
@@ -47,6 +28,7 @@ def add_function(tokens: list, now_token_index: int) -> tuple[float, int]:
         right_token, now_token_index = mul_function(tokens, now_token_index)
         left_token = add_operation(left_token, operation, right_token)
     return left_token, now_token_index
+
 
 def mul_function(tokens: list, now_token_index: int) -> tuple[float, int]:
     left_token, now_token_index = power_function(tokens, now_token_index)
@@ -71,7 +53,7 @@ def mul_function(tokens: list, now_token_index: int) -> tuple[float, int]:
 
 def power_function(tokens: list, now_token_index: int) -> tuple[float, int]:
     left_token, now_token_index = unary_function(tokens, now_token_index)
-    if  now_token_index < len(tokens): # Ставим if, потому что вызываем потом саму функцию еще раз
+    if  now_token_index < len(tokens):
         if tokens[now_token_index] in OPERATORS_POW:
             now_token_index += 1
             right_token, now_token_index = power_function(tokens, now_token_index)
@@ -114,9 +96,11 @@ def mul_operation_float(left_token: float, operator, right_token: float) -> floa
     if operator == '*': return left_token * right_token
     else: return left_token / right_token
 
+
 def mul_operation_integer(left_token: int, operator, right_token: int) -> int:
     if operator == '//': return left_token // right_token
     else: return left_token % right_token
+
 
 def add_operation(left_token, operation, right_token) -> float:
     if operation == '+': return left_token + right_token
